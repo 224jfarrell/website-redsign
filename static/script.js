fc97a79e8c5f2797dcfabeff18c88a1fd9da1fd1
jsoeph farrell <90717706+224jfarrell@users.noreply.github.com>
2023-05-02 12:02:04 -0400

    commit iv - i think

--- static/script.js
+++ static/script.js
@@ -1,4 +1,20 @@
-var password = document.getElementById('password').ariaValueMax;
-for(i=0; i<password.length; i++){
-    
+let password;
+function updatePassword(){
+    var update = document.getElementById('password-r');
+    password = update.value;
+    console.log(password); // DELETE THIS LINE WHEN YOU VERIFY THAT IT WORKS!!!!!!!!
+}
+
+let username;
+function updateUsername(){
+    var update = document.getElementById('username-r');
+    username = update.value;
+    console.log(username); // DELETE THIS LINE WHEN YOU VERIFY THAT IT WORKS!!!!!!!!
+}
+
+let email;
+function updateEmail(){
+    var update = document.getElementById('email');
+    email = update.value;
+    console.log(email); // DELETE THIS LINE WHEN YOU VERIFY THAT IT WORKS!!!!!!!!
 }
