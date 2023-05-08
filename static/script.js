90f7c09c40497aa43435e5a5ecc6cbe9f405d63f
jsoeph farrell <90717706+224jfarrell@users.noreply.github.com>
2023-05-08 09:31:13 -0400

    commit ix

--- static/script.js
+++ static/script.js
@@ -1,23 +1,7 @@
-let password;
-function updatePassword(){
-    var update = document.getElementById('password-r');
-    password = update.value;
-    console.log(password); // DELETE THIS LINE WHEN YOU VERIFY THAT IT WORKS!!!!!!!!
-    localStorage.set('password', password)
+function clearPassword(){
+    document.getElementById('password-r').value = '';
 }
 
-let username;
-function updateUsername(){
-    var update = document.getElementById('username-r');
-    username = update.value;
-    console.log(username); // DELETE THIS LINE WHEN YOU VERIFY THAT IT WORKS!!!!!!!!
-    localStorage.set('username', username);
-}
-
-let email;
-function updateEmail(){
-    var update = document.getElementById('email');
-    email = update.value;
-    console.log(email); // DELETE THIS LINE WHEN YOU VERIFY THAT IT WORKS!!!!!!!!
-    localStorage.set('email', email);
+function clearUsername(){
+    document.getElementById('username-r').value = '';
 }
