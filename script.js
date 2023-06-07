ad99490b78a4479317b2a79576bfbf64a0358036
224jfarrell <224jfarrell@frhsd.com>
2023-06-07 09:26:38 -0400

    commit xviii

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function getPrice(){
                     var t = Math.ceil((tx + st)*10)/10
                     document.getElementById("subtotal").innerHTML = "Subtotal = $" + st;
                     document.getElementById("tax").innerHTML = "Tax = $" + tx;
-                    document.getElementById("total").innerHTML = "Tax = $" + t;
+                    document.getElementById("total").innerHTML = "Total = $" + t;
                 } else if(e[i].value == 2){
                     var st = 99 * f.value;
                     var tx = Math.ceil((st * .07)*10)/10;
@@ -29,12 +29,21 @@ function creditCard(){
     for(i = 0; i < e.length; i++){
         if(e[i].checked){
             if(e[i].value == 16){
-                document.getElementById('visa').disabled = false;
-                document.getElementById('amex').disabled = true;
+                document.getElementById('visa-num').disabled = false;
+                document.getElementById('visa-cvv').disabled = false;
+                document.getElementById('amex-num').disabled = true;
+                document.getElementById('amex-cvv').disabled = true;
             } else if(e[i].value == 15){
-                document.getElementById('visa').disabled = true;
-                document.getElementById('amex').disabled = false;
+                document.getElementById('visa-num').disabled = true;
+                document.getElementById('visa-cvv').disabled = true;
+                document.getElementById('amex-num').disabled = false;
+                document.getElementById('amex-cvv').disabled = false;
             }
         }
     }
+}
+
+function redirect(){
+    alert("Thank You for Choosing Sea Cove Motel! Thank you for choosing our motel for your upcoming stay. We can't wait to welcome you to our cozy and inviting establishment. We have taken every measure to ensure a comfortable and enjoyable stay for you. Our rooms are meticulously prepared, combining modern amenities and a cozy ambiance to create the perfect retreat.If you have any further questions or require any assistance, please feel free to contact our friendly front desk staff, available 24/7. We are here to address any inquiries or accommodate any special requests you may have.");
+    window.location.replace('index.html');
 }
